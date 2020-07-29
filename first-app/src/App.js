5fb45225f7d31fd697dfc8df04a8fd25f416584f
kingsley <ajaezokingsley@gmail.com>
2020-07-29 09:49:50 +0100

    inital commit

---
+++ first-app/src/App.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function App(){
+
+//   return (
+
+//     <div className="App">
+//       <ul>
+//       <li>Somadina</li>
+//       <li>Chinonso</li>
+//       <li>Chiboy</li>
+//     </ul>
+//   </div>
+//   )
+    
+  
+//   }
+  
+
+
+
+// export default App;
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function MyInfo(){
+  return(
+    // An H1 tag with my Name
+    <div className="App">
+      <h1>Ajaezo Kingsley Ezenwa</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
+        desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p>
+      <ul>
+        <li>A visit to London Bridge</li>
+        <li>A visit to Nike lake</li>
+        <li>A visit to a swimming pool</li>
+      </ul>
+    </div>
+  )
+}
+
+
+export default MyInfo;
